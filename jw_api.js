810ad5fa5aaa46bfe5a631aa3cad8d7389ee719d
faunaweblab <digital@faunalab.com>
2020-05-29 14:49:25 +0100

    ...

--- jw_api.js
+++ jw_api.js
@@ -1,15 +1,18 @@
 function generateNounce() {
     let nounce = Math.floor(Math.random() * 100000000);
     console.log(nounce);
+    return nounce;
 }
 
 function getTimestamp() {
     let timeStamp = Math.floor(Date.now() /1000);
     console.log (timeStamp)
+    return timeStamp;
 }
 
 function encodeText() {
     let text = $('#text').val();
    text = encodeURI(text);
    console.log(text);
+   return text;
 }
